70605ce9bea8495d797a5a0e6948afcf34b180e1
Shaxriyor06 <shaxriyorgiyasov66@mail.ru>
2023-10-06 16:18:50 +0500

    lesson 1 js exercises

---
+++ main.js
@@ -0,0 +1,138 @@
+// TASK 1
+
+// let a = prompt("enter the size of side for square");
+// const P = a * 4;
+// alert(P);
+
+// TASK 2
+// const a = prompt("enter the number");
+// const S = a ** 2;
+// alert(S);
+
+// TASK 3
+// let a = prompt("enter the width of triangle");
+// let b = prompt("enter the height of triangle");
+// const P = 2 * a + 2 * b;
+// const S = a * b;
+// alert(`Perimetr equals to ${P} and the square is ${S} `);
+//
+
+// TASK 4
+// const pi = 3.14;
+// let d = prompt("enter the diametr");
+// const L = pi * d;
+// alert(L);
+
+// TASK 5
+// let a = prompt("enter the number");
+// const V = a ** 3;
+// const S = 6 * a ** 2;
+// alert(V);
+// alert(S);
+
+// TASK 6
+// let a = prompt("enter the first side of paralleliped");
+// let b = prompt("enter the second side of paralleliped");
+// let c = prompt("enter the third side of paralleliped");
+// const V = a * b * c;
+// const S = 2 * (a * b + b * c + a * c);
+// alert(S);
+// alert(V);
+
+// TASK 7
+// let R = prompt("enter the radius of circle");
+// const pi = 3.14;
+// const L = 2 * pi * R;
+// const S = pi * R ** 2;
+// alert(L);
+// alert(S);
+
+// TASK 8
+// let a = prompt("enter the first number");
+// let b = prompt("enter the second number");
+// const x = (a + b) / 2;
+// alert(x);
+
+/////////////////////////////////////////////////////////////////////////////////////////////////
+// INTEGER 1
+// let L = prompt("width");
+// const M = L / 100;
+// alert(M);
+
+// INTEGER 2
+// let M = prompt("weight kilogramm");
+// const T = M / 1000;
+// alert(T);
+
+// INTEGER 3
+// let Bites = prompt("REM on bites");
+// const Kb = Bites / 1024;
+// alert(Kb);
+
+// INTEGER 4
+// let a = prompt("enter the first main number");
+// let b = prompt("enter the second number");
+// if (a > b) {
+//     const x = a / b;
+//     alert(`it can be divided in ${x} parts`);
+// } else {
+//     alert(`wrong numbers`);
+// }
+
+// INTEGER 5
+// let a = prompt("enter the first main number");
+// let b = prompt("enter the second number");
+// if (a > b) {
+//     const x = a % b;
+//     alert(`here is the some extra place ${x}`);
+// } else {
+//     alert(`wrong numbers`);
+// }
+
+// INTEGER 6
+// let x = prompt("enter the number between 10 and 99");
+// const b = x % 10;
+// const a = x / 10 - b / 10;
+// alert(`first half of number is equals to ${a}`);
+// alert(`second half of number is equals to ${b}`);
+
+// INTEGER 7
+// let x = prompt("enter the number between 10 and 99");
+// const b = x % 10;
+// const a = x / 10 - b / 10;
+// alert(a + b);
+
+// INTEGER 8
+// let x = prompt("enter the number between 10 and 99");
+// const b = x % 10;
+// const a = x / 10 - b / 10;
+// alert(`the reverce format is ${b}${a}`);
+
+// INTEGER 19
+// let n = prompt(`time in seconds since the day`);
+// const m = n / 60;
+// alert(`it is approximately ${m} minutes`);
+
+// INTEGER 20
+// let n = prompt(`time in seconds since the day`);
+// const m = n / 60;
+// const h = m / 60;
+// alert(`it is approximately ${h} hours`);
+
+// INTEGER 21
+// let n = prompt(`time in seconds since the day`);
+// const m = n / 60;
+// const h = m / 60;
+// alert(`it is approximately ${h} hours and ${m} minutes`);
+
+// INTEGER 22
+// let n = prompt(`time in seconds since the day`);
+// const m = n / 60;
+// const h = m / 60;
+// alert(`it is approximately ${h} hours and ${n} seconds`);
+
+// INTEGER 23
+// let n = prompt(`time in seconds since the day`);
+// const m = n / 60;
+// const h = m / 60;
+// alert(`it is approximately ${h} hours, ${m} minutes and ${n} seconds `);
